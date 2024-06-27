1c3904d542b21767d7c7b8210a42e618ffdc4967
peter rimaszecsi <hull@localappliancerentals.co.uk>
2024-06-27 18:38:55 +0000

    Add update, create and view record functionality

--- employeeapp/urls.py
+++ employeeapp/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('logout/', views.logout, name='logout'),
+    path('add-job/', views.add_job, name='add-job'),
+    path('update-job/<str:pk>/', views.update_job, name='update-job'),
+    path('single-job/<str:pk>/', views.single_job, name='single-job'),
 ]
--- employeeapp/views.py
+++ employeeapp/views.py
@@ -34,5 +34,31 @@ def dashboard(request):
     context = {'job_done': job_done}
     return render(request, 'employeeapp/dashboard.html', context=context)
 
+@login_required(login_url='login')
+def add_job(request):
+    if request.method == 'POST':
+        job_title = request.POST.get('job_title')
+        job_done_in_hours = request.POST.get('job_done_in_hours')
+        JobsDone.objects.create(worker=request.user, job_title=job_title, job_done_in_hours=job_done_in_hours)
+        return redirect('dashboard')
+    return render(request, 'employeeapp/create-record.html')
+
+@login_required(login_url='login')
+def update_job(request, pk):
+    job = JobsDone.objects.get(id=pk)
+    form = JobsDone(instance=job)
+    if request.method == 'POST':
+        form = JobsDone(request.POST, instance=job)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard')
+    context = {'form': form}
+    return render(request, 'employeeapp/update-record.html', context=context)
+
+@login_required(login_url='login')
+def single_job(request, pk):
+    all_jobs = JobsDone.objects.get(id=pk)
+    context = {'all_jobs': all_jobs}
+    return render(request, 'employeeapp/view-record.html', context=context)
 
 
