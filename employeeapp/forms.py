0ee72b3d0c41391c9f12fa37fac73161dc5d26bd
peter rimaszecsi <hull@localappliancerentals.co.uk>
2024-06-27 10:20:08 +0000

    Create forms.py
    Add CustomUserCreationForm and LoginForm

---
+++ employeeapp/forms.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from django.forms.widgets import PasswordInput, TextInput
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
+        widgets = {
+            'username': TextInput(attrs={'class': 'form-control'}),
+            'password1': PasswordInput(attrs={'class': 'form-control'}),
+            'password2': PasswordInput(attrs={'class': 'form-control'}),
+        }
+        
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=TextInput(attrs={'class':'validate','placeholder': 'Username'}))
+    password = forms.CharField(widget=PasswordInput(attrs={'placeholder':'Password'}))
