42c83fbca2d1200bf79c7c491f1ce8a7fdef6ff6
peter rimaszecsi <hull@localappliancerentals.co.uk>
2024-06-27 08:40:38 +0000

    Change function name

--- clockin/urls.py
+++ clockin/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from employeeapp.views import employee
+from employeeapp.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
