f753719453f05433a6a31e29de68346a7f245103
peter rimaszecsi <hull@localappliancerentals.co.uk>
2024-06-27 10:45:08 +0000

    Add login functionality to login.html

--- employeeapp/urls.py
+++ employeeapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name=''),
+    path('login/', views.login, name='login'),
 ]
--- employeeapp/views.py
+++ employeeapp/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+from employeeapp.forms import LoginForm
+from django.contrib.auth.models import auth
+
 
 def home(request):
     return render(request, 'employeeapp/index.html')
-    #return HttpResponse("Hello, world. You're at the employeeapp index.")
+
+def login(request):
+    form = LoginForm()
+    
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)  
+            
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    context = {'form': form}
+    return render(request, 'employeeapp/login.html', context=context) 
