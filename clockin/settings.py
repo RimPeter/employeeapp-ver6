e9d90dcb4b49411b4ddd9e948a4da0e566e39d20
peter rimaszecsi <hull@localappliancerentals.co.uk>
2024-06-27 09:56:26 +0000

    Add templates
    base.html, index.html

--- clockin/settings.py
+++ clockin/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b#0e6y8&p^!ds8vfv+*&v$q^l(wt)ojsc2(lihb=-&9t5a$$a@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['http://localhost:8000/', 'localhost', '.herokuapp.com']
 
@@ -47,7 +47,7 @@ ROOT_URLCONF = 'clockin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['static'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -112,10 +112,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-BASE_DIR / "static",
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- employeeapp/views.py
+++ employeeapp/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, There!")
+    return render(request, 'employeeapp/index.html')
+    #return HttpResponse("Hello, world. You're at the employeeapp index.")
