7826eee197caac16b38bb5e5e701dc5c5ad727af
peter rimaszecsi <hull@localappliancerentals.co.uk>
2024-06-28 10:57:52 +0000

    Add UpdateJobForm and CreateJobForm to form.py

--- employeeapp/forms.py
+++ employeeapp/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import JobsDone
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from django.forms.widgets import PasswordInput, TextInput
@@ -17,3 +18,21 @@ class CustomUserCreationForm(UserCreationForm):
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=TextInput(attrs={'class':'validate','placeholder': 'Username'}))
     password = forms.CharField(widget=PasswordInput(attrs={'placeholder':'Password'}))
+    
+class UpdateJobForm(forms.ModelForm):
+    class Meta:
+        model = JobsDone
+        fields = ['job_title', 'job_done_in_hours']
+        widgets = {
+            'job_title': forms.Select(attrs={'class': 'form-control'}),
+            'job_done_in_hours': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
+        
+class CreateJobForm(forms.ModelForm):
+    class Meta:
+        model = JobsDone
+        fields = ['job_title', 'job_done_in_hours']
+        widgets = {
+            'job_title': forms.Select(attrs={'class': 'form-control'}),
+            'job_done_in_hours': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
