a9eab5f74fcae748e79bfe85ff680a58ee0e8fde
peter rimaszecsi <hull@localappliancerentals.co.uk>
2024-06-29 12:04:56 +0000

    Modify model

--- employeeapp/models.py
+++ employeeapp/models.py
@@ -30,22 +30,10 @@ class JobsDone(models.Model):
     
 
 class Employee(models.Model):
-    select_user = models.ForeignKey(User, on_delete=models.CASCADE)
-    firstname = models.CharField(max_length=50)
-    lastname = models.CharField(max_length=50)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length=15)
-    address = models.CharField(max_length=100)
-    city = models.CharField(max_length=50)
-    postcode = models.CharField(max_length=10)
-    
-    def __str__(self):
-        return self.firstname + ' ' + self.lastname
-    
-    # name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
 
-    # def __str__(self):
-    #     return self.name
+    def __str__(self):
+        return self.name
 
 class ClockIn(models.Model):
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
@@ -53,4 +41,4 @@ class ClockIn(models.Model):
     clock_out_time = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
-        return f"{Employee} - {self.clock_in_time} - {self.clock_out_time}"
+        return f"{self.employee.name} - {self.clock_in_time} - {self.clock_out_time}"
