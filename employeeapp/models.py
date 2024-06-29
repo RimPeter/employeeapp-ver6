d03c3eb8f1ea62bb1d107d655af39e5b7a891dc1
peter rimaszecsi <hull@localappliancerentals.co.uk>
2024-06-29 10:56:34 +0000

    Modify model Employee

--- employeeapp/models.py
+++ employeeapp/models.py
@@ -31,14 +31,12 @@ class JobsDone(models.Model):
 
 class Employee(models.Model):
     name = models.CharField(max_length=100)
-    # Add other fields as necessary
 
     def __str__(self):
         return self.name
 
 class ClockIn(models.Model):
-    #employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    employee = models.ForeignKey(User, on_delete=models.PROTECT)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
     clock_in_time = models.DateTimeField(default=timezone.now)
     clock_out_time = models.DateTimeField(null=True, blank=True)
 
